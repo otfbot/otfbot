9fef86513ceb9001102a0d8f49f838dc6612280d
cato- <cato-@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2006-08-08 22:41:22 +0000

    * Added systematic logging to console and files * Added draft for a config in xml
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@2 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- configMod.py
+++ configMod.py
@@ -6,22 +6,23 @@ import random, re
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation; either version 2 of the License, or
 #(at your option) any later version.
-
+#
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-#You should have received a copy of the GNU General Public License along
-#with this program; if not, write to the Free Software Foundation, Inc.,
-#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 class chatMod:
 	def __init__(self, bot):
 		self.bot=bot
 
 	def msg(self, user, channel, msg):
+		user=user.split("!")[0]		
 		if self.bot.auth(user) and channel==self.bot.nickname:
-			user=user.split("!")[0]
 			if msg[0:6] == "config":
 				msg=msg[7:]
 				if msg[0:3] == "get":
