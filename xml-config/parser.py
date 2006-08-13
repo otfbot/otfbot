dfa96dcbddde67d9a5cb5a6143eb73da452996aa
cato- <cato-@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2006-08-13 19:19:50 +0000

    * finished xml-to-array-parser
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@5 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- xml-config/parser.py
+++ xml-config/parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from xml.sax import saxutils
-import string
+import string, pprint
 
 def normalize_whitespace(text):
     "Remove redundant whitespace from a string"
@@ -14,6 +14,7 @@ class ParseConfig(saxutils.DefaultHandler):
 		self.current_mod=""
 		self.op=-1
 		self.net=""
+		self.chan=""
 	
 	def startElement(self, name, attrs):
 		self.current.append(str(name))
@@ -22,26 +23,39 @@ class ParseConfig(saxutils.DefaultHandler):
 		if name == "general":
 			self.config['general']={}
 		if name == "network":
-			self.config['network']={attrs.get('name',''):{}}
+			if 'network' not in self.config:
+				self.config['network'] = {}
+			self.config['network'][attrs.get('name','')] = {}
 			self.net=attrs.get('name','')
+		if name == "channel":
+			if 'channel' not in self.config['network'][self.net]:
+				self.config['network'][self.net]['channel'] = {}
+			self.config['network'][self.net]['channel'][attrs.get('name','')] = {}
+			self.chan = attrs.get('name','')
 		if self.current[-1] == "modul":
 			if self.current[1] == "general":
 				if not 'module' in self.config[self.current[1]]: 
 					self.config[self.current[1]]['module'] = {}
 				if not self.current_mod in self.config[self.current[1]]['module']: 
 					self.config[self.current[1]]['module'][self.current_mod]={}
-			if self.current[1] == "network":
+			elif self.current[1] == "network" and not self.current[2] == "channel":
 				if not 'module' in self.config[self.current[1]][self.net]:
 					self.config[self.current[1]][self.net]['module'] = {}
 				if not self.current_mod in self.config[self.current[1]][self.net]['module']:
 					self.config[self.current[1]][self.net]['module'][self.current_mod] = {}
+			elif self.current[1] == "network" and self.current[2] == "channel":
+				if not 'module' in self.config[self.current[1]][self.net]['channel'][self.chan]:
+					self.config[self.current[1]][self.net]['channel'][self.chan]['module'] = {}
+				if not self.current_mod in self.config[self.current[1]][self.net]['channel'][self.chan]['module']:
+					self.config[self.current[1]][self.net]['channel'][self.chan]['module'][self.current_mod] = {}
+
 		if name == "param" and self.current[-2] == "modul":
 			if self.current[1] == "general":
 				mod=self.config[self.current[1]]['module'][self.current_mod]
-			elif self.current[1] == "network":
+			elif self.current[1] == "network" and not self.current[2] == "channel":
 				mod=self.config[self.current[1]][self.net]['module'][self.current_mod]
-			else:
-				mod={}
+			elif self.current[1] == "network" and self.current[2] == "channel":
+				mod=self.config[self.current[1]][self.net]['channel'][self.chan]['module'][self.current_mod]
 			name = str(attrs.get('name',''))
 			if name in mod:
 				if type(mod[name]) != 'list':
@@ -52,10 +66,12 @@ class ParseConfig(saxutils.DefaultHandler):
 				mod[name].append(attrs.get('value',''))
 			else:
 				mod[attrs.get('name','')] = attrs.get('value','')
-			if self.current[-3] == "general":
+			if self.current[1] == "general":
 				self.config[self.current[1]]['module'][self.current_mod] = mod
-			elif self.current[-3] == "network":
+			elif self.current[1] == "network" and not self.current[2] == "channel":
 				self.config[self.current[1]][self.net]['module'][self.current_mod] = mod
+			elif self.current[1] == "network" and self.current[2] == "channel":
+				self.config[self.current[1]][self.net]['channel'][self.chan]['module'][self.current_mod] = mod
 		if name == "operator":
 			self.op=self.op+1
 			if not 'operator' in self.config[self.current[1]]: 
@@ -63,19 +79,27 @@ class ParseConfig(saxutils.DefaultHandler):
 			self.config[self.current[1]]['operator'].append({})
 	
 	def characters(self, ch):
-		if len(self.current) == 3:
-			if self.current[2] == "nickname":
-				self.config[self.current[1]]['nickname'] = normalize_whitespace(ch)
-			elif self.current[2] == "realname":
-				self.config[self.current[1]]['realname'] = normalize_whitespace(ch)
+		if len(self.current) == 3 and self.current[2] in ('nickname','realname','server','port'):
+			if self.current[1] == "general":
+				self.config[self.current[1]][self.current[2]] = normalize_whitespace(ch)
+			if self.current[1] == "network":
+				self.config[self.current[1]][self.net][self.current[2]] = normalize_whitespace(ch)
 		if len(self.current) == 4 and self.current[2] == "operator":
 			self.config[self.current[1]]['operator'][self.op][self.current[3]] = normalize_whitespace(ch)
-			
+		if self.current[-1] == "enable" and normalize_whitespace(ch).lower() == "false":
+			if self.current[1] == "general":
+				self.config[self.current[1]]['module'][self.current_mod] = {'disabled': 'disabled'}
+			if self.current[1] == "network" and not self.current[2] == "channel":
+				self.config[self.current[1]][self.net]['module'][self.current_mod] = {'disabled': 'disabled'}
+			if self.current[1] == "network" and self.current[2] == "channel":
+				self.config[self.current[1]][self.net]['channel'][self.chan]['module'][self.current_mod] = {'disabled': 'disabled'}
+ 
 
 	def endElement(self, name):
 		self.current.pop()
 		if name == "chatbot":
-			print self.config
+			pp = pprint.PrettyPrinter(width=1)
+			pp.pprint(self.config)
 
 from xml.sax import make_parser
 from xml.sax.handler import feature_namespaces
