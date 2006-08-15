aff24240a2dcd0d02cdada9e1539e3881d28c1f4
allo <allo@laxu.de>
2006-08-15 14:52:25 +0000

    xml export
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@11 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- xml-config/parser2.py
+++ xml-config/parser2.py
@@ -1,103 +1,133 @@
 #!/usr/bin/python
 import handyxml
 
-generic_options={}
-network_options={}
-channel_options={}
 
-def getoptions(optionlist):
-	ret={}
-	for option in optionlist:
+
+class config:
+	def __init__(self, filename):
+		"""Initialize the config class and load a xml config"""
+		self.generic_options={}
+		self.network_options={}
+		self.channel_options={}
+		
+		self.doc=handyxml.xml(filename)
+		#generic options
+		options={}
 		try:
-			ret[option.name]=option.value
+			options=self.doc.option
 		except AttributeError:
-			print "Config Error: Option name or value missing"
-	return ret
+			pass
+		self.generic_options=self.getoptions(options)
 	
-def getsuboptions(list):
-	ret={}
-	for item in list:
+		#network specific
+		networks=[]
 		try:
-			options=item.option
+			networks=self.doc.network
 		except AttributeError:
 			pass
-		tmp=getoptions(options)
-		try:
-			ret[item.name]=tmp
-		except AttributeError:
-			print "Config Error: network/channel has no name"
-	return ret
-
-def get(option, default, network=None, channel=None):
-		try:
-			return channel_options[network][channel][option]
-		except KeyError:
-			pass
-		try:
-			return network_options[network][option];
-		except KeyError:
-			pass
-		try:
-			return generic_options[option];
-		except KeyError:
-			pass
+		self.network_options=self.getsuboptions(networks);
+	
+		#channel specific (format = channel_options[network][channel])
+		for network in networks:
+			channels=[]
+			try:
+				channels=network.channel
+			except AttributeError:
+				pass
+			try:
+				self.channel_options[network.name]=self.getsuboptions(channels)
+			except AttributeError:
+				print "Config Error: network has no name"
+	
+	def getoptions(self, optionlist):
+		ret={}
+		for option in optionlist:
+			try:
+				ret[option.name]=option.value
+			except AttributeError:
+				print "Config Error: Option name or value missing"
+		return ret
+		
+	def getsuboptions(self, list):
+		ret={}
+		for item in list:
+			try:
+				options=item.option
+			except AttributeError:
+				pass
+			tmp=self.getoptions(options)
+			try:
+				ret[item.name]=tmp
+			except AttributeError:
+				print "Config Error: network/channel has no name"
+		return ret
+	
+	def get(self, option, default, network=None, channel=None):
+			try:
+				return self.channel_options[network][channel][option]
+			except KeyError:
+				pass
+			try:
+				return self.network_options[network][option];
+			except KeyError:
+				pass
+			try:
+				return self.generic_options[option];
+			except KeyError:
+				pass
+			#set default in config, and return it
+			if network and channel:
+				if not network in self.channel_options.keys():
+					self.channel_options[network]={}
+				if not channel in self.channel_options[network].keys():
+					self.channel_options[network][channel]={}
+				self.channel_options[network][channel][option]=default
+			elif network:
+				if not network in self.network_options.keys():
+					self.network_options[network]={}
+				self.network_options[network][option]=default
+			else:
+				self.generic_options[option]=default
+			return default
+	
+	def set(self, option, value, network=None, channel=None):
 		if network and channel:
-			if not network in channel_options.keys():
-				channel_options[network]={}
-			if not channel in channel_options[network].keys():
-				channel_options[network][channel]={}
-			channel_options[network][channel][option]=default
+			if not network in self.channel_options.keys():
+				self.channel_options[network]={}
+			if not channel in self.channel_options[network].keys():
+				self.channel_options[network][channel]={}
+			self.channel_options[network][channel][option]=value
 		elif network:
-			if not network in network_options.keys():
-				network_options[network]={}
-			network_options[network][option]=default
+			if not network in self.network_options.keys():
+				self.network_options[network]={}
+			self.network_options[network][option]=value
 		else:
-			generic_options[option]=default
-		return default
+			self.generic_options[option]=value
+				
 
-def set(option, value, network=None, channel=None):
-	if network and channel:
-		if not network in channel_options.keys():
-			channel_options[network]={}
-		if not channel in channel_options[network].keys():
-			channel_options[network][channel]={}
-		channel_options[network][channel][option]=value
-	elif network:
-		if not network in network_options.keys():
-			network_options[network]={}
-		network_options[network][option]=value
-	else:
-		generic_options[option]=value
-			
-def loadfromxml(filename):
-	doc=handyxml.xml(filename)
-	#generic options
-	options=[]
-	try:
-		options=doc.option
-	except AttributeError:
-		pass
-	generic_options=getoptions(options)
-
-	#network specific
-	networks=[]
-	try:
-		networks=doc.network
-	except AttributeError:
-		pass
-	network_options=getsuboptions(networks);
-
-	#channel specific (format = channel_options[network][channel])
-	for network in networks:
-		channels=[]
-		try:
-			channels=network.channel
-		except AttributeError:
-			pass
-		try:
-			channel_options[network.name]=getsuboptions(channels)
-		except AttributeError:
-			print "Config Error: network has no name"
+	def getxml(self):
+		ret="<?xml version=\"1.0\"?>\n"
+		ret+="<chatbot>\n"
+		indent="	";
+		#generic options
+		for option in self.generic_options.keys():
+			ret+=indent+"<option name=\""+option+"\" value=\""+self.generic_options[option]+"\" />\n"
+		#network specific
+		channel_networks=self.channel_options.keys() #get the networks, for which we have channel settings
+		for network in self.network_options.keys():
+			ret+=indent+"<network name=\""+network+"\">\n"
+			for option in self.network_options[network].keys():
+				ret+=indent*2+"<option name=\""+option+"\" value=\""+self.network_options[network][option]+"\" />\n"
+			if network in channel_networks:
+				for channel in self.channel_options[network].keys():
+					ret+=indent*2+"<channel name=\""+channel+"\">\n"
+					for option in self.channel_options[network][channel].keys():
+						ret+=indent*3+"<option name=\""+option+"\" value=\""+self.channel_options[network][channel][option]+"\" />\n"
+					ret+=indent*2+"</channel>\n"
+			ret+=indent+"</network>\n"
+		
+		ret+="</chatbot>\n"
+		return ret
 
 def test_get():
 	print "Test:"
@@ -113,6 +143,9 @@ def test_get():
 	print ""
 	print get(setting, default, network, channel)
 
-	
-loadfromxml("config2.xml")
+
+myconfig=config("config2.xml")
+print myconfig.getxml()
+#print generic_options
+#print getxml();
 #test_get()
