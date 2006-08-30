5a3cacc88eff023de63272243ababa8f0b8184ee
allo <allo@laxu.de>
2006-08-30 17:33:57 +0000

    multichannel commands
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@28 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- commandsMod.py
+++ commandsMod.py
@@ -1,11 +1,11 @@
 # This file is part of OtfBot.
 #
-# OtfBot is free software; you can redistribute it and/or modify
+# Otfbot is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# OtfBot is distributed in the hope that it will be useful,
+# Otfbot is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -17,60 +17,64 @@
 # (c) 2005, 2006 by Alexander Schier
 #
 
-import string, re, functions
-import chatMod
+import string, re
+import chatMod, functions
 
 def default_settings():
 	settings={};
-	settings['commandsMod_fileencoding']='iso-8859-15'
-	settings['commandsMod_file']='commands.txt'
+	settings['commandsMod.fileencoding']='iso-8859-15'
+	settings['commandsMod.file']='commands.txt'
+	settings['commandsmod.commandChar']='!'
 	return settings
 		
 class chatMod(chatMod.chatMod):
 	def __init__(self, bot):
 		self.bot = bot
-		self.commandsFile=bot.getConfig("commandsMod_file", "commands.txt")
-		self.commands = functions.loadProperties(self.commandsFile)
+
+	def connectionMade(self):
+		self.reload()
 		
 	def joined(self, channel):
 		self.channel = channel
 	
 	def msg(self, user, channel, msg):
 		user = user.split("!")[0] #only nick
-		if msg[0] == "!":
-			if msg == "!reload-commands":
+		if msg[0] == self.commandChar[channel]:
+			if msg == "!reload-commands": #TODO: global methodChar
 				self.logger.info("reloading")
 				self.reload()
 				return
-			answer = self.respond(user, msg)
+			answer = self.respond(channel, user, msg)
 			if answer != "":
-				self.bot.sendme(channel, answer, self.bot.getConfig("commandsMod_fileencoding", "iso-8859-15"))
+				self.bot.sendme(channel, answer, self.bot.getConfig("commandsMod.fileencoding", "iso-8859-15"))
 			
 	def reload(self):
-		self.commands = functions.loadProperties(self.commandsFile)
+		self.commands={}
+		for channel in self.bot.channels:
+			tmp=functions.loadProperties(self.bot.getConfig("file", "commands.txt", "commandsMod", self.bot.network, channel))
+			self.commands[channel]=tmp
+			self.commandChar[channel]=self.bot.getConfig("commandChar", "!", "commandsMod", self.bot.network, channel)
 
-	def respond(self, user, command):
+	def getCommand(self, channel, cmd):
+		if not self.commands.has_key(channel):
+			self.commands[channel]={}
+		if self.commands[channel].has_key(cmd):
+			return self.command[channel][cmd]
+		return None
+
+	def respond(self, channel, user, command):
 		answer = ""
-		for key in self.commands.keys():
-			#print command[1:len(key)]
-			action = string.lower(command[1:len(key)+1])
-			#TODO: "key_" without "key"
-			if action == string.lower(key):
-				if string.lower(command[1:]) == key: #exact match
-					#return self.commands[key]
-					#answer = re.sub("USER", user, self.commands[key])
-					answer = self.commands[key]
-					answer = re.sub("USER", user, answer)
-				else: #with nick?
-					if self.commands.has_key(key+"_"):
-						otherNick = command[len(key)+1:]
-						otherNick = otherNick[1:] #strip whitespace between command and nick
-						if len(otherNick)>0 and otherNick[-1] == " ": #strip whitespace at end (if any)
-							otherNick = otherNick[0:-1]
-							
-						answer = self.commands[key+"_"]
-						answer = re.sub("USER", user, answer)
-						answer = re.sub("OTHER", otherNick, answer)
+		if command[0] == self.commandChar[channel]:
+			tmp=command[1:].split(" ", 1)
+			cmd=string.lower(tmp[0])
+			if len(tmp) >1:
+				param=tmp[1]
+				answer=getCommand(channel, cmd+"_")
+				answer = re.sub("OTHER", param, answer)
+			else:
+				answer=getCommand(channel, cmd)
+			answer = re.sub("USER", user, answer)
+				
 		if len(answer)>0 and answer[-1] == "\n":
 			return answer[0:-1]
 		else:
