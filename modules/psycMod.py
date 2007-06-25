6b07f8b689c5125134c489a0bdfa9f9fd2a3e23b
allo <allo@laxu.de>
2007-06-25 21:57:46 +0000

    allow admins to send psyc commands to the bot, which the bot should send prefixed with the psyccmdchar to the psyc-server
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@125 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

---
+++ modules/psycMod.py
@@ -0,0 +1,37 @@
+# OtfBot module to allow the botowner to control the bot with psyc commands
+#
+# Otfbot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Otfbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OtfBot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# (c) 2007 by Alexander Schier
+#
+
+import string, re
+import chatMod, functions
+
+def default_settings():
+	settings={};
+	settings['psycmod.psyccommand']='!psyc'
+	settings['psycmod.psyccmdchar']='!psyc'
+	return settings
+		
+class chatMod(chatMod.chatMod):
+	def __init__(self, bot):
+		self.bot = bot
+
+	def msg(self, user, channel, msg):
+		user = user.split("!")[0] #only nick
+		if self.bot.auth(user) > 5 and msg[:len(self.bot.getConfig('psycmod.psyccommand', '!psyc'))]:
+			msg=msg[len(self.bot.getConfig('psycmod.psyccommand', '!psyc')):]
+			self.bot.sendmsg(channel, self.bot.getConfig('psycmod.psyccmdchar', '+')+msg[1:])
