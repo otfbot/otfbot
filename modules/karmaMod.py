b3f347878b39c24ce5b913bf0a2ad3bf042408e3
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-02-12 17:00:45 +0000

    Freestyle Syntax, no hardcoded paths
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@146 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- modules/karmaMod.py
+++ modules/karmaMod.py
@@ -24,30 +24,66 @@ class chatMod(chatMod.chatMod):
 	def default_settings():
 		settings={};
 		settings['karmaMod.file']=datadir+'/karma.txt'
+		settings['karmaMod.freestyle']="true"
+		settings['karmaMod.verbose']="true"
 		return settings
 
 	def __init__(self, bot):
 		self.bot=bot
 		self.karma={}
-		self.karma=functions.loadProperties(self.bot.getConfig("file", datadir+"/karma.txt", "karmaMod", self.bot.network))
+		self.karma=functions.loadProperties(self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", datadir+"/karma.txt"), "karmaMod", self.bot.network))
+		self.verbose=self.bot.getBoolConfig("karmaMod.verbose", "true")
+		self.freestyle=self.bot.getBoolConfig("karmaMod.freestyle", "true")
 
 	def command(self, user, channel, command, options):
-		if command == "karma":
+		if self.freestyle:
+			if options[-2:]=="++":
+				what=command+" "+options[:-2]
+				self.karma_up(what)
+				self.tell_karma(what, channel)
+			elif options[-2:]=="--":
+				what=command+" "+options[:-2]
+				self.karma_down(what)
+				self.tell_karma(what, channel)
+			elif command[-2:]=="++":
+				self.karma_up(command[:-2])
+				self.tell_karma(command[:-2], channel)
+			elif command[-2:]=="--":
+				self.karma_down(command[:-2])
+				self.tell_karma(command[:-2], channel)
+		elif command == "karma":
 			if options == "":
 				self.bot.sendmsg(channel, "Nutzen: !karma name++ oder !karma name--")
 			else:
-				if (options[-2:]=="++" or options[-2:]=="--") and not options[:-2] in self.karma.keys():
-					self.karma[options[:-2]]=0
-				elif not options in self.karma.keys():
-					self.karma[options]=0
 				if options[-2:]=="++":
-					self.karma[options[:-2]]=self.karma[options[:-2]]+1
+					self.karma_up(options[:-2])
+					if self.verbose:
+						self.tell_karma(options[:-2], channel)
 				elif options[-2:]=="--":
-					self.karma[options[:-2]]=self.karma[options[:-2]]-1
+					self.karma_down(options[:-2])
+					if self.verbose:
+						self.tell_karma(options[:-2], channel)
 				else:
-					self.bot.sendmsg(channel, "Karma: "+options+": "+str(self.karma[options])) 
+					self.tell_karma(options, channel)
+	def tell_karma(self, what, channel):
+		self.bot.sendmsg(channel, "Karma: "+what+": "+str(self.get_karma(what))) 
+	def get_karma(self, what):
+		if not what in self.karma.keys():
+			self.karma[what]=0
+		return self.karma[what]
+	def do_karma(self, what, up):
+		if not what in self.karma.keys():
+			self.karma[what]=0
+		if up:
+			self.karma[what]=int(self.karma[what])+1
+		else:
+			self.karma[what]=int(self.karma[what])-1
+	def karma_up(self, what):
+		return self.do_karma(what, True)
+	def karma_down(self, what):
+		return self.do_karma(what, False)
 	def connectionLost(self, reason):
-		file=open(self.bot.getConfig("file", datadir+"/karma.txt", "karmaMod", self.bot.network), "w")
+		file=open(self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", datadir+"/karma.txt"), "karmaMod", self.bot.network), "w")
 		for user in self.karma.keys():
 			file.write(user+"="+str(self.karma[user])+"\n")
 		file.close()
