5983ddc314339ea4adacd41bc847b7fbe45f24a9
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-03-04 13:52:49 +0000

    karma with user and reason. TODO: nice commands like !why-karmadown, !why-karmaup, !who- ...
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@159 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- modules/karmaMod.py
+++ modules/karmaMod.py
@@ -18,66 +18,95 @@
 #
 
 import chatMod
-import functions
+import os.path,pickle
 
 class chatMod(chatMod.chatMod):
 	def __init__(self, bot):
 		self.bot=bot
 		self.karma={}
-		self.karma=functions.loadProperties(datadir+self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", "/karma.txt"), "karmaMod", self.bot.network))
+		karmapath=datadir+datadir+"/"+self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", "karma.dat"), "karmaMod", self.bot.network)
+		if os.path.exists(karmapath):
+			karmafile=open(karmapath, "r")
+			self.karma=pickle.load(karmafile)
+			karmafile.close()
 		self.verbose=self.bot.getBoolConfig("karmaMod.verbose", "true")
 		self.freestyle=self.bot.getBoolConfig("karmaMod.freestyle", "true")
 
 	def command(self, user, channel, command, options):
+		up=False
+		what=None
+		reason=None
+		tmp=options.split("#",1)
+		options=tmp[0].strip()
+		if len(tmp)==2:
+			reason=tmp[1]
+
 		if self.freestyle:
 			if options[-2:]=="++":
+				up=True
 				what=command+" "+options[:-2]
-				self.karma_up(what)
-				self.tell_karma(what, channel)
 			elif options[-2:]=="--":
+				up=False
 				what=command+" "+options[:-2]
-				self.karma_down(what)
-				self.tell_karma(what, channel)
 			elif command[-2:]=="++":
-				self.karma_up(command[:-2])
-				self.tell_karma(command[:-2], channel)
+				up=True
+				what=command[:-2]
 			elif command[-2:]=="--":
-				self.karma_down(command[:-2])
-				self.tell_karma(command[:-2], channel)
+				up=False
+				what=command[:-2]
+
 		elif command == "karma":
 			if options == "":
 				self.bot.sendmsg(channel, "Nutzen: !karma name++ oder !karma name--")
+				return
 			else:
 				if options[-2:]=="++":
-					self.karma_up(options[:-2])
-					if self.verbose:
-						self.tell_karma(options[:-2], channel)
+					up=True
+					what=options[:-2]
 				elif options[-2:]=="--":
-					self.karma_down(options[:-2])
-					if self.verbose:
-						self.tell_karma(options[:-2], channel)
+					up=False
+					what=options[:-2]
 				else:
 					self.tell_karma(options, channel)
+					return
+		#Execute it
+		self.do_karma(what, up, reason, user)
+		if self.verbose:
+			self.tell_karma(what, channel)
 	def tell_karma(self, what, channel):
 		self.bot.sendmsg(channel, "Karma: "+what+": "+str(self.get_karma(what))) 
 	def get_karma(self, what):
 		if not what in self.karma.keys():
-			self.karma[what]=0
-		return self.karma[what]
-	def do_karma(self, what, up):
+			self.karma[what]=[0,{},{},[],[]] #same as below!
+		return self.karma[what][0]
+	def do_karma(self, what, up, reason, user):
+		user=user.split("!")[0]
 		if not what in self.karma.keys():
-			self.karma[what]=0
+			self.karma[what]=[0,{},{},[],[]] #score, who-up, who-down, why-up, why-down
 		if up:
-			self.karma[what]=int(self.karma[what])+1
+			self.karma[what][0]=int(self.karma[what][0])+1
+			if not user in self.karma[what][1].keys():
+				self.karma[what][1][user]=1
+			else:
+				self.karma[what][1][user]+=1
+			if reason:
+				self.karma[what][3].append(str(reason))
 		else:
-			self.karma[what]=int(self.karma[what])-1
-	def karma_up(self, what):
-		return self.do_karma(what, True)
-	def karma_down(self, what):
-		return self.do_karma(what, False)
+			self.karma[what][0]=int(self.karma[what][0])-1
+			if not user in self.karma[what][2].keys():
+				self.karma[what][2][user]=-1
+			else:
+				self.karma[what][2][user]-=1
+			if reason:
+				self.karma[what][4].append(str(reason))
+	def karma_up(self, what, reason=None):
+		return self.do_karma(what, True, reason)
+	def karma_down(self, what, reason=None):
+		return self.do_karma(what, False, reason)
 	def connectionLost(self, reason):
-		file=open(datadir+self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", "/karma.txt"), "karmaMod", self.bot.network), "w")
-		for user in self.karma.keys():
-			file.write(user+"="+str(self.karma[user])+"\n")
-		file.close()
+		karmafile=open(datadir+"/"+self.bot.getConfig("file", self.bot.getConfig("karmaMod.file", "karma.dat"), "karmaMod", self.bot.network), "w")
+		#for user in self.karma.keys():
+		#	file.write(user+"="+str(self.karma[user])+"\n")
+		pickle.dump(self.karma, karmafile)
+		karmafile.close()
 
