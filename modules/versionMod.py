716852e77db64543537dfa7dba7687581116156d
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-05-20 20:56:55 +0000

    simple version output TODO: when we get versions, output "version x.y (svn rXXXX)", using a version constant in otfbot.py
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@189 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

---
+++ modules/versionMod.py
@@ -0,0 +1,28 @@
+# This file is part of OtfBot.
+#
+# OtfBot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# OtfBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OtfBot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# (c) 2008 by Alexander Schier
+
+import random, re
+import chatMod
+
+class chatMod(chatMod.chatMod):
+	def __init__(self, bot):
+		self.bot=bot
+
+	def command(self, user, channel, command, options):
+		if command == "version":
+				self.bot.sendmsg(channel, "r"+str(self.bot.svnrevision))
