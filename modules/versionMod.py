28f2bf59d64b87705d5115020311ae463b0bc32f
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-08-06 16:58:52 +0000

    humanMod now sends bot-generated messages to the client + loaded but disabled by default
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@226 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- modules/versionMod.py
+++ modules/versionMod.py
@@ -29,7 +29,7 @@ class chatMod(chatMod.chatMod):
 		(query,t) = messages[0]
 		answer = None
 		if query == "VERSION":
-			answer = "OtfBot SVN r"+str(self.bot.svnrevision)
+			answer = "mirc"
 		if answer: 
 			self.bot.ctcpMakeReply(user.split("!")[0], [(query,answer)])
 			self.logger.info("Answered to CTCP "+query+" Request from "+user.split("!")[0])
