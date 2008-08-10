d9c0acbbdea222b3f6990e289448bf1fbd2405a1
allo <allo@laxu.de>
2008-08-10 16:04:17 +0000

    testrunner
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@244 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

---
+++ test.py
@@ -0,0 +1,21 @@
+import sys
+from optparse import OptionParser
+sys.path.insert(1,"lib") # Path for auxilary libs of otfbot
+sys.path.insert(1,"modules")
+
+parser=OptionParser()
+(options, args)=parser.parse_args()
+if len(args)!=1:
+	print "usage: test.py someMod"
+	sys.exit(1)
+
+import doctest
+try:
+	module=__import__(args[0])
+	finder=doctest.DocTestFinder()
+	runner=doctest.DebugRunner(verbose=True)
+	for test in finder.find(module):
+		runner.run(test)
+except ImportError:
+	print "import error for module \"%s\""%args[0]
+
