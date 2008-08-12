100bd11864a1a27913f56c0310e1f12fd65d95cc
allo <allo@laxu.de>
2008-08-12 20:05:28 +0000

    added more mirrored actions
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@282 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- modules/mirrorMod.py
+++ modules/mirrorMod.py
@@ -28,3 +28,42 @@ class chatMod(chatMod.chatMod):
 		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
 			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
 			self.bot.ipc[target_network].sendmsg(target_channel, ("< %s> "+msg)%user.split("!")[0])
+	def action(self, user, channel, message):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "* %s %s "%(user.split("!")[0], message))
+	def kickedFrom(self, channel, kicker, message):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s was kicked from %s by %s [%s]"%(self.bot.nickname, channel, kicker, message))
+	def userKicked(self, kickee, channel, kicker, message):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s was kicked from %s by %s [%s]"%(kickee, channel, kicker, message))
+	def userJoined(self, user, channel):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s has joined %s"%(user.split("!")[0], channel))
+	def userLeft(self, user, channel):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s has left %s"%(user.split("!")[0], channel))
+	def userQuit(self, user, channel):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s has quit %s"%(user.split("!")[0], channel))
+	def userRenamed(self, oldname, newname):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "%s is now known as %s"%(oldname, newname))
+	def modeChanged(self, user, channel, set, modes, args):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			sign="+"
+			if not set:
+				sign="-"
+			self.bot.ipc[target_network].sendmsg(target_channel, "mode/"+channel+" ["+sign+modes+" "+" ".join(args)+"] by "+user.split("!")[0])
+	def joined(self, channel):
+		if (self.network, channel) in self.bot.hasConfig("mirrorto", "mirrorMod")[2]:
+			(target_network, target_channel)=self.bot.getConfig("mirrorto", "unset", "mirrorMod", self.network, channel).split("-", 1)
+			self.bot.ipc[target_network].sendmsg(target_channel, "joined %s"%(channel))
