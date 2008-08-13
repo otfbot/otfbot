b23ae895db3fdab14c55a05860b9d160d4a4bb2b
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-08-13 21:29:07 +0000

    serverMod: fix for stop, if no client was connected, yet. otfbot.py: main-function instead of global commands, start bot only if __name__=="__main__"
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@303 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- otfbot.py
+++ otfbot.py
@@ -26,87 +26,149 @@ from twisted.internet import reactor, ssl
 sys.path.insert(1,"lib") # Path for auxilary libs of otfbot
 import functions, config
 from botfactory import BotFactory
-
-# some constants for paths, might be read from configfile in future
-path_log="otfbot.log"
-path_errorlog="error.log"
-path_pid="otfbot.pid"
-path_cfg="etc/otfbot.yaml"
-path_mods="modules"
-path_data="data"
-
-# Path for bot-modules
-sys.path.insert(1,path_mods)
-
-###############################################################################
-# Parse commandline options
 import cmdlineparser
-(options, args) = cmdlineparser.parse()
-###############################################################################
-#check for root rights
-if os.getuid()==0:
-	if options.userid and options.userid!=0 and options.groupid and options.groupid!=0:
-		from twisted.python.util import switchUID
-		#os.chroot(".") #DOES NOT WORK. pwd.getpwuid fails in switchUID, when chrooted.
-		switchUID(options.userid, options.groupid)
-	else:
-		print "Otfbot should not be run as root."
-		print "please use -u and -g to specify a userid/groupid"
-		sys.exit(1)
-###############################################################################
-# Detaching from console
-if options.foreground == False and not options.debug > 0:
-	try:
-		import subprocess
-		subprocess.Popen(["python",sys.argv[0],"-f"])
-		sys.exit(0)
-	except ImportError:
-		pass
-###############################################################################
-# Setup Logging
+
+#logging
 import logging
 import logging.handlers
 from twisted.python import log
 import log as otfbotlog
-# Basic settings for logging
-# logging to logfile
-filelogger = logging.handlers.RotatingFileHandler(path_log,'a',1048576,5)
-errorlogger = logging.handlers.RotatingFileHandler(path_errorlog,'a',1048576,5)
-errorlogger.setLevel(logging.ERROR)
-#filelogger = logging.FileHandler(path_log,'a')
-logging.getLogger('').setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s %(name)-18s %(module)-18s %(levelname)-8s %(message)s')
-filelogger.setFormatter(formatter)
-errorlogger.setFormatter(formatter)
-logging.getLogger('').addHandler(filelogger)
-logging.getLogger('').addHandler(errorlogger)
-#corelogger.addHandler(filelogger)
 
-if options.debug > 0:
-	# logging to stdout
-	console = logging.StreamHandler()
-	logging.getLogger('').setLevel(options.debug)
-	formatter = logging.Formatter('%(asctime)s %(name)-10s %(module)-18s %(levelname)-8s %(message)s')
-	console.setFormatter(formatter)
-	logging.getLogger('').addHandler(console)
-	#corelogger.addHandler(console)
-corelogger = logging.getLogger('core')
-corelogger.info("  ___ _____ _____ ____        _   ")
-corelogger.info(" / _ \_   _|  ___| __ )  ___ | |_ ")
-corelogger.info("| | | || | | |_  |  _ \ / _ \| __|")
-corelogger.info("| |_| || | |  _| | |_) | (_) | |_ ")
-corelogger.info(" \___/ |_| |_|   |____/ \___/ \__|")
-corelogger.info("")
-svnrevision="$Revision$".split(" ")[1]
-corelogger.info("Starting OtfBot - Version svn "+svnrevision)
-# get messages from twisted as well
-plo=otfbotlog.PythonLoggingObserver()
-plo.start()
+def main():
+	# some constants for paths, might be read from configfile in future
+	path_cfg="etc/otfbot.yaml"
+	path_mods="modules"
 
+	###############################################################################
+	# Parse commandline options
+	(options, args) = cmdlineparser.parse()
+	###############################################################################
+	# config
+	theconfig=None
+	if options.configfile != None:
+		configfile=options.configfile
+	else:
+		configfile=path_cfg
+	modulesconfigdir=path_mods #TODO: configuration-option(?)
+	theconfig=config.loadConfig(configfile, modulesconfigdir)
+	new_config=False
+	if not theconfig: #file could not be loaded, i.e at first start
+		theconfig=config.config()
+		theconfig.set('enabled', 'false', 'main', 'samplenetwork')
+		theconfig.set('server', 'localhost', 'main', 'samplenetwork')
+		theconfig.set('enabled', 'false', 'main', 'samplenetwork', '#example')
+		theconfig.set('nickname', 'OtfBot', 'main')
+		theconfig.set('encoding', 'UTF-8', 'main')
+		#theconfig.set('pidfile','otfbot.pid','main')
+			
+		new_config=True
+
+	###############################################################################
+	# some settings
+	path_log=theconfig.getConfig("logfile", "otfbot.log", "main")
+	path_errorlog=theconfig.getConfig("errorlog", "error.log", "main")
+	path_pid=theconfig.getConfig("pidfile", "otfbot.pid", "main")
+	path_data=theconfig.getConfig("datadir", "data", "main")
+	###############################################################################
+	# Setup Logging
+
+	# logging to logfile
+	filelogger = logging.handlers.RotatingFileHandler(path_log,'a',1048576,5)
+	errorlogger = logging.handlers.RotatingFileHandler(path_errorlog,'a',1048576,5)
+	errorlogger.setLevel(logging.ERROR)
+	#filelogger = logging.FileHandler(path_log,'a')
+	logging.getLogger('').setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s %(name)-18s %(module)-18s %(levelname)-8s %(message)s')
+	filelogger.setFormatter(formatter)
+	errorlogger.setFormatter(formatter)
+	logging.getLogger('').addHandler(filelogger)
+	logging.getLogger('').addHandler(errorlogger)
+	#corelogger.addHandler(filelogger)
+	if options.debug > 0:
+		# logging to stdout
+		console = logging.StreamHandler()
+		logging.getLogger('').setLevel(options.debug)
+		formatter = logging.Formatter('%(asctime)s %(name)-10s %(module)-18s %(levelname)-8s %(message)s')
+		console.setFormatter(formatter)
+		logging.getLogger('').addHandler(console)
+		#corelogger.addHandler(console)
+	corelogger = logging.getLogger('core')
+	corelogger.info("  ___ _____ _____ ____        _   ")
+	corelogger.info(" / _ \_   _|  ___| __ )  ___ | |_ ")
+	corelogger.info("| | | || | | |_  |  _ \ / _ \| __|")
+	corelogger.info("| |_| || | |  _| | |_) | (_) | |_ ")
+	corelogger.info(" \___/ |_| |_|   |____/ \___/ \__|")
+	corelogger.info("")
+	svnrevision="$Revision$".split(" ")[1]
+	corelogger.info("Starting OtfBot - Version svn "+svnrevision)
+	# get messages from twisted as well
+	plo=otfbotlog.PythonLoggingObserver()
+	plo.start()
+	###############################################################################
+	# Load modules
+	classes=[]
+	# Path for bot-modules
+	sys.path.insert(1,path_mods)
+	for file in os.listdir(path_mods):
+		if len(file)>=3 and file[-3:]==".py":
+			classes.append(__import__(file[:-3]))
+			#classes[-1].datadir = "modules/"+classes[-1].__name__+"-data"
+			classes[-1].datadir = path_data+"/"+classes[-1].__name__
+			corelogger.debug("Loading module "+classes[-1].__name__)
+	###############################################################################
+
+	createClassList(theconfig, classes) #auto create new classlist, if the classlist is removed by the user or on new config
+	theconfig.writeConfig(configfile) #mostly needed for new configs
+
+	if new_config:
+		#no logger here: the user needs to read this on console, not in logfile
+		print "Default Settings loaded."
+		print "Edit "+configfile+" to configure the bot."
+		sys.exit(0)
+
+
+	# writing PID-File
+	pidfile=path_pid
+	f=open(pidfile,'w')
+	f.write(str(os.getpid())+"\n")
+	f.close()
+	###############################################################################
+
+	#check for root rights
+	if os.getuid()==0:
+		if options.userid and options.userid!=0 and options.groupid and options.groupid!=0:
+			from twisted.python.util import switchUID
+			#os.chroot(".") #DOES NOT WORK. pwd.getpwuid fails in switchUID, when chrooted.
+			switchUID(options.userid, options.groupid)
+		else:
+			print "Otfbot should not be run as root."
+			print "please use -u and -g to specify a userid/groupid"
+			sys.exit(1)
+	###############################################################################
+	# Detaching from console
+	if options.foreground == False and not options.debug > 0:
+		try:
+			import subprocess
+			subprocess.Popen(["python",sys.argv[0],"-f"])
+			sys.exit(0)
+		except ImportError:
+			pass
+
+	ipc=InstanceCommunication(theconfig, classes)
+
+	###############################################################################
+	# registering the exithook function
+	reactor.addSystemEventTrigger('before','shutdown',disconnect, ipc, corelogger)
+	reactor.addSystemEventTrigger('during','shutdown',exithook, theconfig, pidfile, configfile, corelogger)
+
+
+	for network in theconfig.getNetworks():
+		ipc.connectNetwork(network)
+	reactor.run()
 
 ###############################################################################
 
-def exithook():
+def exithook(theconfig, pidfile, configfile, corelogger):
 	""" This function is called, when the program terminates. """
 	# remove Pidfile
 	os.remove(pidfile)
@@ -114,71 +176,27 @@ def exithook():
 	corelogger.info("Bot shutted down")
 	corelogger.info("-------------------------")
 
-# Load modules
-classes=[]
-for file in os.listdir(path_mods):
-	if len(file)>=3 and file[-3:]==".py":
-		classes.append(__import__(file[:-3]))
-		#classes[-1].datadir = "modules/"+classes[-1].__name__+"-data"
-		classes[-1].datadir = path_data+"/"+classes[-1].__name__
-		corelogger.debug("Loading module "+classes[-1].__name__)
 
-###############################################################################
-# some config functions
-theconfig=None
-
-if options.configfile != None:
-	configfile=options.configfile
-else:
-	configfile=path_cfg
-modulesconfigdir=path_mods #TODO: configuration-option(?)
-theconfig=config.loadConfig(configfile, modulesconfigdir)
-
-def createClassList():
+def createClassList(theconfig, classes):
 	if theconfig.hasConfig("modsEnabled", "main")[0]==False: #TODO: how to handle if only classes have module-lists?
 		theconfig.set("modsEnabled", [i.__name__ for i in classes], "main")
 
-if not theconfig: #file could not be loaded, i.e at first start
-	theconfig=config.config()
-	theconfig.set('enabled', 'false', 'main', 'samplenetwork')
-	theconfig.set('server', 'localhost', 'main', 'samplenetwork')
-	theconfig.set('enabled', 'false', 'main', 'samplenetwork', '#example')
-	theconfig.set('nickname', 'OtfBot', 'main')
-	theconfig.set('encoding', 'UTF-8', 'main')
-	theconfig.set('pidfile','otfbot.pid','main')
-		
-	createClassList() #because of the system.exit we need this here, too
-	theconfig.writeConfig(configfile)
-	#no logger here: the user needs to read this on console, not in logfile
-	print "Default Settings loaded."
-	print "Edit "+configfile+" to configure the bot."
-	sys.exit(0)
-
-createClassList() #auto create new classlist, if the classlist is removed by the user
-
-# writing PID-File
-pidfile=theconfig.getConfig('pidfile',path_pid,'main')
-f=open(pidfile,'w')
-f.write(str(os.getpid())+"\n")
-f.close()
 
 
-def disconnect():
+def disconnect(ipc, corelogger):
 	corelogger.info("Disconnecting from all networks")
 	for i in ipc.getall():
 		corelogger.info("Disconnecting from "+i)
 		ipc.get(i).disconnect()
 	reactor.doIteration(0)
 
-# registering the exithook function
-reactor.addSystemEventTrigger('before','shutdown',disconnect)
-reactor.addSystemEventTrigger('during','shutdown',exithook)
-
 
 class InstanceCommunication:
-	def __init__(self):
+	def __init__(self, theconfig, classes):
 		self.ins={}
+		self.theconfig=theconfig
 		self.logger=logging.getLogger("ipc")
+		self.classes=classes
 	def add(self,name,inst):
 		self.ins[name]=inst
 	def rm(self,name):
@@ -191,19 +209,15 @@ class InstanceCommunication:
 		corelogger.info(str(self.ins))
 	def getall(self):
 		return self.ins
-	def connectNetwork(self,network):
-		self.logger.info("Trying to connect to "+network)
-		if(theconfig.getBoolConfig('enabled', 'unset', 'main', network)):
-			f = BotFactory(network, self, theconfig, classes)
-			servername=theconfig.getConfig("server", "localhost", "main", network)
-			if (theconfig.getBoolConfig('ssl','False','main',network)):
+	def connectNetwork(self, network):
+		if(self.theconfig.getBoolConfig('enabled', 'unset', 'main', network)):
+			self.logger.info("Trying to connect to "+network)
+			f = BotFactory(network, self, self.theconfig, self.classes)
+			servername=self.theconfig.getConfig("server", "localhost", "main", network)
+			if (self.theconfig.getBoolConfig('ssl','False','main',network)):
 				s = ssl.ClientContextFactory()
-				reactor.connectSSL(servername, int(theconfig.getConfig('port','6697','main',network)), f,s)
+				reactor.connectSSL(servername, int(self.theconfig.getConfig('port','6697','main',network)), f,s)
 			else:
-				reactor.connectTCP(servername, int(theconfig.getConfig('port','6667','main',network)), f)
-		
-ipc=InstanceCommunication()
-
-for network in theconfig.getNetworks():
-	ipc.connectNetwork(network)
-reactor.run()
+				reactor.connectTCP(servername, int(self.theconfig.getConfig('port','6667','main',network)), f)
+if __name__=="__main__":
+	main()
