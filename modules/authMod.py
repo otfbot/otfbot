f6b54a5b054a815c28a4d6446d7929eec7224a57
samoht <samoht@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2008-09-07 19:00:49 +0000

    Added userdb for password authentification
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@340 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- modules/authMod.py
+++ modules/authMod.py
@@ -15,6 +15,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 # (c) 2005, 2006 by Alexander Schier
+# (c) 2008 by Thomas Wiegart
 #
 
 import random, re
@@ -23,19 +24,45 @@ import chatMod
 class chatMod(chatMod.chatMod):
 	def __init__(self, bot):
 		self.bot=bot
-		self.whitelist=[]
-		for user in self.bot.getConfig("whitelist", "", "authMod").split(","):
-			self.whitelist.append(user)
-		self.password=self.bot.getConfig("password", "", "authMod")
+		self.users={}
 
 	def query(self, user, channel, msg):
-		user=user.split("!")[0]
-		if msg[0:9] == "identify " and self.password != "" and msg[9:] == self.password:
-			self.bot.sendmsg(user, "Password accepted")
-			self.logger.info("User "+str(user)+" successfully identified with password")
-			self.whitelist.append(user)
+		"""
+		Authentification for some access-levels.
+		User, password and access level can be found in the userdb.
+		in default the path to the userdb is data/authMod/userdb.yaml,
+		but you can specify your own path by setting the authMod.userdb
+		option in the config file to your path.
+		
+		The user-file has to look like this:
+			"
+			networkname:
+			  username: password(md5-hash!),access-level
+			"
+		You can specify as many networks and users as you want.
+		"""
+		#user=user.split("!")[0]
+		import yaml,md5
+		if msg[0:9] == "identify ":
+			yamlfile = open(self.bot.getConfig("userdb","data/authMod/userdb.yaml","authMod"),"r")
+			a = ""
+			for i in yamlfile.readlines():
+				a = a + i
+			users = yaml.load(a)
+			users = users[self.network]
+			if users[msg.split(" ")[1].lower()].split(",")[0] == md5.md5(msg.split(" ",2)[2]).hexdigest():
+				self.bot.sendmsg(user.split("!")[0], "Password accepted")
+				self.logger.info("User "+str(user)+" successfully identified with password as user " + str(msg.split(" ")[1]))
+				self.users[user] = int(users[msg.split(" ")[1].lower()].split(",")[1])
+				self.bot.sendmsg(user.split("!")[0], "You has been identifyed as user " + str(msg.split(" ")[1]) + " with access level " + str(users[msg.split(" ")[1].lower()].split(",")[1]))
+				self.logger.info("User "+str(user)+" now has access level " + str(users[msg.split(" ")[1].lower()].split(",")[1]))
 
 	def auth(self, user):
-		user=user.split("!")[0]
-		if user in self.whitelist:
-			return 10
+		#user=user.split("!")[0]
+		"""
+		Returns the access-level of the given user.
+		"""
+		try:
+			return self.users[user]
+		except:
+			return 0
