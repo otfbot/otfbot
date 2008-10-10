054ccfcbfdb65e79ade670d10895c9eae9e21899
allo <allo@laxu.de>
2008-10-10 18:09:05 +0000

    identicaMod
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/branches/0.9dev@415 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

---
+++ modules/identicaMod.py
@@ -0,0 +1,44 @@
+# This file is part of OtfBot.
+#
+# OtfBot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# OtfBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OtfBot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# (c) 2008 by Alexander Schier
+
+import chatMod
+
+LIB=True
+try:
+	import identi
+except ImportError:
+	LIB=False
+
+class chatMod(chatMod.chatMod):
+	def __init__(self, bot):
+		self.bot=bot
+	def connectionMade(self):
+		if not LIB:
+			self.logger.info("please download http://media.commandline.org.uk/code/identi.txt to lib/identi.py to use identicaMod")
+			return
+
+	def command(self, user, channel, command, options):
+		if not LIB:
+			return
+		if command in ["identica", "i", "identicawithnick", "iwn"]:
+			self.api=identi.IdentiCA(self.bot.getConfig("username", '', 'identicaMod', self.bot.network), self.bot.getConfig("username", '', 'identicaMod', self.bot.network))
+			self.api.login()
+			if command=="iwn" or command=="identicawithnick":
+				options=user.split("!")[0]+": "+options
+			options=options[:140]
+			self.api.put_message(options)
