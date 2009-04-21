ad3d87f0ad0768110448a5caa16b91b8c662b20b
cato- <otfbot@xenim.de>
2009-04-21 12:22:43 +0000

    fixed generateconfig to be run from the helpers-dir
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@551 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- helpers/generateconfig.py
+++ helpers/generateconfig.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python
 import sys, glob
-sys.path.insert(1, "services")
-sys.path.insert(1, "../services")
+base="../"
+sys.path.insert(1, base+"services")
 import config as configService
 
-config=configService.configService("otfbot.yaml")
+config=configService.configService(base+"otfbot.yaml")
 
-files=glob.glob("plugins/ircClient/*.py")
+files=glob.glob(base+"plugins/ircClient/*.py")
 modules=[]
 for file in files:
-	plugin=file.split("plugins/ircClient/")[1].split(".py")[0]
+	plugin=file.split(base+"plugins/ircClient/")[1].split(".py")[0]
 	if not plugin=="__init__":
 		modules.append(plugin)
 config.set("ircClientPlugins", modules, 'main')
 
-files=glob.glob("plugins/ircServer/*.py")
+files=glob.glob(base+"plugins/ircServer/*.py")
 modules=[]
 for file in files:
-	plugin=file.split("plugins/ircServer/")[1].split(".py")[0]
+	plugin=file.split(base+"plugins/ircServer/")[1].split(".py")[0]
 	if not plugin=="__init__":
 		modules.append(plugin)
 config.set("ircServerPlugins", modules, 'main')
 
-files=glob.glob("services/*.py")
+files=glob.glob(base+"services/*.py")
 modules=[]
 for file in files:
-	plugin=file.split("services/")[1].split(".py")[0]
+	plugin=file.split(base+"services/")[1].split(".py")[0]
 	if not plugin=="__init__" and not plugin=="config":
 		modules.append(plugin)
 config.set("services", modules, 'main')
