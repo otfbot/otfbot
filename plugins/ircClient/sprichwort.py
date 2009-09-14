f690438fabd854febaaf73a7ec1c3317e771f2d7
allo <allo@laxu.de>
2009-09-14 16:09:08 +0000

    Thx to sprichwortrekombinator.de
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@603 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

---
+++ plugins/ircClient/sprichwort.py
@@ -0,0 +1,33 @@
+# This file is part of OtfBot.
+#
+# OtfBot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# OtfBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OtfBot; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# (c) 2009 by Alexander Schier
+#
+
+import urllib2, re
+from lib import chatMod
+
+class Plugin(chatMod.chatMod):
+    def __init__(self, bot):
+        self.bot=bot
+
+    def command(self, user, channel, command, options):
+        if command == "sprichwort":
+            url=urllib2.urlopen("http://www.sprichwortrekombinator.de")
+            data=url.read()
+            url.close()
+            sprichwort=re.search("<div class=\"spwort\">([^<]*)<\/div>", data, re.S).group(1)
+            self.bot.sendmsg(channel, sprichwort)
