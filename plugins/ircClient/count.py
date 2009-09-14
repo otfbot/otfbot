c0f3ecac652ec36e286af1a4c7971b1a179d117b
allo <allo@caf4e4cc-781a-0410-a5c3-d4e744a0d9f4>
2009-09-14 09:20:11 +0000

    improved lpm
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/otfbot/trunk@602 caf4e4cc-781a-0410-a5c3-d4e744a0d9f4

--- plugins/ircClient/count.py
+++ plugins/ircClient/count.py
@@ -4,20 +4,24 @@ import time
 class Plugin(chatMod.chatMod):
     def __init__(self,bot):
         self.linesperminute={}
-        self.new_linesperminute={}
+        self.new_lines={}
+        self.timestamp={}
         self.minute={}
     def msg(self, user, channel, msg):
-        new_minute=int(time.strftime("%M"))
-        if not self.minute.has_key(channel) or self.minute[channel] != new_minute:
-            self.minute[channel]=new_minute
-            if not self.new_linesperminute.has_key(channel):
-                self.new_linesperminute[channel]=0
-            self.linesperminute[channel]=self.new_linesperminute[channel]
-            self.new_linesperminute[channel]=0
-        else:
-            self.new_linesperminute[channel]+=1
+        self.calcLPM(channel)
+        self.new_lines[channel]+=1
+    def calcLPM(self, channel):
+        new_timestamp=int(time.time())
+        if not self.timestamp.has_key(channel):
+            self.timestamp[channel]=new_timestamp
+        if not self.new_lines.has_key(channel):
+            self.new_lines[channel]=0
+        if new_timestamp - self.timestamp[channel] >= 60:
+            self.linesperminute[channel]=int(self.new_lines[channel]*60.0/(new_timestamp - self.timestamp[channel]))
+            self.new_lines[channel]=0
+            self.timestamp[channel]=new_timestamp
     def getLinesPerMinute(self, channel):
-        if self.linesperminute.has_key(channel):
-            return self.linesperminute[channel]
-        else:
-            return 0
+        self.calcLPM(channel)
+        if not self.linesperminute.has_key(channel):
+            self.linesperminute[channel]=0
+        return self.linesperminute[channel]
