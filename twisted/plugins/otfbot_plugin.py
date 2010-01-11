03cb3c1f72820d7b8d65376fc271ef7a4f4245ec
cato <cato-@mail.berlios.de>
2010-01-11 19:14:43 +0100

    Moved logging to twisted-plugin, removed .tac

--- twisted/plugins/otfbot_plugin.py
+++ twisted/plugins/otfbot_plugin.py
@@ -1,15 +1,39 @@
+#!/usr/bin/twistd -noy
+# -*- coding: iso-8859-1 -*-
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+# (c) 2005 - 2010 by Alexander Schier
+# (c) 2006 - 2010 by Robert Weidlich
+# 
+
+from twisted.application import internet, service
+from twisted.application.service import IServiceMaker
+from twisted.plugin import IPlugin
+from twisted.python import log, usage
+import twisted
+
 from zope.interface import implements
 
-from twisted.python import usage
-from twisted.plugin import IPlugin
-from twisted.application.service import IServiceMaker
-from twisted.application import internet, service
+import logging, logging.handlers, sys
 
-from otfbot.services import config as configService
 from otfbot.lib import version
+from otfbot.services import config as configService
 
 class Options(usage.Options):
-        optParameters = [["config","c","otfbot.yaml","Location of configfile"]]
+    optParameters = [["config","c","otfbot.yaml","Location of configfile"]]
 
 class MyServiceMaker(object):
     implements(IServiceMaker, IPlugin)
@@ -31,6 +55,54 @@ class MyServiceMaker(object):
             sys.exit(1)
         configS.setServiceParent(application)
 
+        ################################################################################
+        ## Begin of logging setup
+        
+        logging.getLogger('').setLevel(logging.DEBUG)
+        root=logging.getLogger('')
+        
+        formatPattern = configS.get("format", '%(asctime)s %(name)-10s %(module)-14s %(funcName)-20s %(levelname)-8s %(message)s', "logging")
+        formatter = logging.Formatter(formatPattern)
+        
+        logfile = configS.get("file", False, "logging")
+        if logfile:
+            filelogger = logging.handlers.RotatingFileHandler(logfile,'a',1048576,5)
+            filelogger.setFormatter(formatter)
+            root.addHandler(filelogger)
+        errfile = configS.get("errfile", False, "logging")
+        if errfile:        
+            errorlogger = logging.handlers.RotatingFileHandler(errfile,'a',1048576,5)
+            errorlogger.setFormatter(formatter)
+            errorlogger.setLevel(logging.ERROR)
+            root.addHandler(errorlogger)
+        memorylogger = logging.handlers.MemoryHandler(1000)
+        memorylogger.setFormatter(formatter)
+        root.addHandler(memorylogger)
+        stdout = configS.get("logToConsole", True, "logging")
+        if stdout:
+            console = logging.StreamHandler()
+            console.setFormatter(formatter)
+            root.addHandler(console)
+        
+        plo = log.PythonLoggingObserver()
+        if twisted.version.minor > 2:
+            log.startLoggingWithObserver(plo.emit)
+        else:
+            plo.start()
+        
+        corelogger = logging.getLogger('core')
+        
+        ## end of logging setup
+        ################################################################################
+
+        corelogger.info("  ___ _____ _____ ____        _   ")
+        corelogger.info(" / _ \_   _|  ___| __ )  ___ | |_ ")
+        corelogger.info("| | | || | | |_  |  _ \ / _ \| __|")
+        corelogger.info("| |_| || | |  _| | |_) | (_) | |_ ")
+        corelogger.info(" \___/ |_| |_|   |____/ \___/ \__|")
+        _v="version %s" % application.version.short()
+        corelogger.info(" "*(34-len(_v))+_v)
+
         service_names=configS.get("services", [], "main")
         service_classes=[]
         service_instances=[]
@@ -41,10 +113,5 @@ class MyServiceMaker(object):
             service_instances[-1].setServiceParent(application)
 
         return application
-
-
-# Now construct an object which *provides* the relevant interfaces
-# The name of this variable is irrelevant, as long as there is *some*
-# name bound to a provider of IPlugin and IServiceMaker.
 
 serviceMaker = MyServiceMaker()
