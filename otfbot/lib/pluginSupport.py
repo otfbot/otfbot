f732e40c3ae71f3ef0d90d638dab1a6cb6253328
allo <allo@laxu.de>
2010-04-12 23:22:15 +0200

    better logmessage

--- otfbot/lib/pluginSupport.py
+++ otfbot/lib/pluginSupport.py
@@ -200,7 +200,7 @@ class pluginSupport:
             @type exception: exception
         """
         if type(exception) == self.DependencyMissing:
-            logger.warning("Dependency missing in plugin %s: %s is not active."%(plugin, str(exception)))
+            logger.warning("Dependency missing in plugin %s: %s. plugin not started."%(plugin, str(exception)))
             return
         elif type(exception) == self.WontStart:
             logger.info('Plugin "%s" will not start because "%s".'%(plugin, str(exception)))
