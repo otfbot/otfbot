900bbba82bb86feee53f1c39ff0fe86547e96ca6
cato- <otfbot@xenim.de>
2010-05-07 19:49:01 +0200

    Style fixes and doc updates

--- otfbot/lib/urlutils.py
+++ otfbot/lib/urlutils.py
@@ -13,34 +13,42 @@
 # You should have received a copy of the GNU General Public License
 # along with OtfBot; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 # (c) 2008 - 2010 by Alexander Schier
 # (c) 2008 - 2010 by Robert Weidlich
 #
 
 import urllib2
 from twisted.web import client
-client.HTTPClientFactory.noisy=False
+client.HTTPClientFactory.noisy = False
 
 from otfbot.lib import version
 
-_version=version._version
+_version = version._version
+
+"""
+   Some functions to simplify handling of HTTP-requests
+"""
+
 
 def get_headers(url):
-    request=urllib2.Request(url)
-    request.get_method=lambda: 'HEAD'
-    http=urllib2.urlopen(request)
+    request = urllib2.Request(url)
+    request.get_method = lambda: 'HEAD'
+    http = urllib2.urlopen(request)
     return http.headers
+
 
 def is_html(headers):
     return headers['content-type'].lower()[:9] == "text/html"
 
+
 def download_headers_and_content_if_html(url):
-    headers=get_headers()
-    content=''
+    headers = get_headers()
+    content = ''
     if is_html(headers):
-        content=download(url)
+        content = download(url)
     return (headers, content)
+
 
 def download_if_html(url):
     """
@@ -50,12 +58,13 @@ def download_if_html(url):
         return download(url)
     return ""
 
+
 def download(url, file=None, **kwargs):
-    """ 
+    """
     Uses twisted.web.client.getPage() to fetch a Page via HTTP
-    
+
     @return: A Defered which will call a Callback with the content as argument
-    
+
     """
     if "agent" not in kwargs:
         kwargs['agent'] = "OTFBot (%s; otfbot.berlios.de)" % _version.short()
