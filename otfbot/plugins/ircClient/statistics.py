4f7ddad876396cf7e044c9993b81e90d4d5f2c93
allo <allo@laxu.de>
2010-05-19 23:52:24 +0200

    merged count and statistics

--- otfbot/plugins/ircClient/statistics.py
+++ otfbot/plugins/ircClient/statistics.py
@@ -21,19 +21,48 @@
 """
 import time
 from otfbot.lib import chatMod
+from otfbot.lib import functions
 
 class Plugin(chatMod.chatMod):
     def __init__(self, bot):
         self.bot=bot
         self.peak={}
         self.peak_date={}
+        self.linesperminute = {}
+        self.new_lines = {}
+        self.timestamp = {}
+
+    def msg(self, user, channel, msg):
+        self.calcLPM(channel)
+        self.new_lines[channel][-1] += 1
+
+    def calcLPM(self, channel):
+        new_timestamp = int(time.time())
+        if not channel in self.timestamp:
+            self.timestamp[channel] = new_timestamp
+        if not channel in self.new_lines:
+            self.new_lines[channel] = [0, 0, 0, 0, 0]
+        no_lines = reduce(lambda x, y: x + y, self.new_lines[channel][:-1])
+        timediff = new_timestamp - self.timestamp[channel]
+        if timediff >0:
+            self.linesperminute[channel] = no_lines * 60 / 4.0 / timediff
+        if timediff >=60:
+            self.new_lines[channel] = self.new_lines[channel][1:]
+            self.new_lines[channel].append(0)
+            self.timestamp[channel] = new_timestamp
+
+    def getLinesPerMinute(self, channel):
+        self.calcLPM(channel)
+        if not channel in self.linesperminute:
+            self.linesperminute[channel] = 0
+        return self.linesperminute[channel]
 
     def joined(self, channel):
         if not channel in self.peak:
             self.peak[channel]=len(self.bot.users[channel])
         if not channel in self.peak_date:
             self.peak_date[channel]=time.strftime("%d.%m.%Y %H:%M")
-        self._recalc_peak()
+        self._recalc_peak(channel)
 
     def userJoined(self, user, channel):
         self._recalc_peak(channel)
@@ -43,7 +72,8 @@ class Plugin(chatMod.chatMod):
             self.peak[channel]=len(self.bot.users[channel])
             self.peak_date[channel]=time.strftime("%d.%m.%Y %H:%M")
 
-
     def command(self, user, channel, command, options):
         if command == "peak":
             self.bot.sendmsg(channel, "Maximale Nutzerzahl (%s) erreicht am %s"%(self.peak[channel], self.peak_date[channel]))
+        elif command == "lpm":
+            self.bot.sendmsg(channel, "aktuelle Zeilen pro Minute: %s"%(self.getLinesPerMinute(channel)))
