a73bec4e35c696ff4f9611e0a758af021ce17813
allo <allo@laxu.de>
2010-08-30 02:18:49 +0200

    xmppClient

---
+++ otfbot/plugins/xmppClient/echo.py
@@ -0,0 +1,25 @@
+from otfbot.lib.pluginSupport.decorators import callback
+from otfbot.lib.pluginSupport import plugin
+
+from twisted.words.xish import domish
+
+class Plugin(plugin.Plugin):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.logger.debug("Hello world")
+    @callback
+    def onMessage(self, msg):
+        if not msg.body:
+            return
+
+        if msg["type"] == 'chat' and hasattr(msg, "body") \
+            and not str(msg.body)[0:4]=="?OTR":
+
+            self.logger.debug(str(msg.body))
+            reply = domish.Element((None, "message"))
+            reply["to"] = msg["from"]
+            reply["from"] = msg["to"]
+            reply["type"] = 'chat'
+            reply.addElement("body", content="echo: " + str(msg.body))
+
+            self.bot.send(reply)
