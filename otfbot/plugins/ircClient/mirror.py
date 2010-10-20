94d117d73e3958b0b1f142e11b12ee27a65b5bab
Farthen <wilkefi@googlemail.com>
2010-10-20 10:54:39 +0000

    Move the docstring of ircClient.mirror to a more appropriate location

--- otfbot/plugins/ircClient/mirror.py
+++ otfbot/plugins/ircClient/mirror.py
@@ -27,14 +27,14 @@ from otfbot.lib import color
 
 
 class Plugin(chatMod.chatMod):
+    """
+        Configuration:
+        If you want to mirror channel #a to channel #b then you need to add
+        mirror.mirrorto: network-#b
+        to the channel configuration of #a  where 'network' is the irc network name.
+    """
 
     def __init__(self, bot):
-        """
-            Configuration:
-            If you want to mirror channel #a to channel #b then you need to add
-            mirror.mirrorto: network-#b
-            to the channel configuration of #a  where 'network' is the irc network name.
-        """
         self.bot = bot
         self.getbot = lambda network: bot.root.getServiceNamed('ircClient').getServiceNamed(network).protocol
         # the bot is red
