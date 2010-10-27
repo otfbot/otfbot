f17239e4f780e029821edb6ada3d4824432300e3
allo <allo@laxu.de>
2010-10-27 22:33:53 +0200

    § is not in ascii

--- otfbot/plugins/ircClient/ki.py
+++ otfbot/plugins/ircClient/ki.py
@@ -95,7 +95,7 @@ def ascii_string(msg):
             pass
         except UnicodeEncodeError:
             pass
-    return re.sub("[ ]+", " ", re.sub("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@.!?;:/%\$§\-_ ]", " ", msg))
+    return e.sub("[ ]+", " ", re.sub("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@.!?;:/%\$\-_ ]", " ", msg))
 
 class udpResponder(responder):
     def __init__(self, bot):
