548efac1f579951aad30ff87f2b1d5f2f9fffa74
cato <otfbot@xenim.de>
2011-04-15 19:38:57 +0200

    Made url plugin fully asyncronious

--- otfbot/lib/urlutils.py
+++ otfbot/lib/urlutils.py
@@ -26,38 +26,87 @@ import urllib2
 from twisted.web import client
 client.HTTPClientFactory.noisy = False
 
+from twisted.internet import defer
 from otfbot.lib import version
-
+from twisted.internet import reactor
 _version = version._version
 
+from twisted.web.client import _parse
+
+def get_page_with_header(url, contextFactory=None, *args, **kwargs):
+    """
+    Download a web page as a string.
+
+    Download a page. Return a deferred, which will callback with a
+    page (as a string) or errback with a description of the error.
+
+    See HTTPClientFactory to see what extra args can be passed.
+    """
+    if type(url) == unicode:
+        url = url.encode("utf-8")
+    scheme, host, port, path = _parse(url)
+    factory = client.HTTPClientFactory(url, *args, **kwargs)
+    if scheme == 'https':
+        from twisted.internet import ssl
+        if contextFactory is None:
+            contextFactory = ssl.ClientContextFactory()
+        reactor.connectSSL(host, port, factory, contextFactory)
+    else:
+        reactor.connectTCP(host, port, factory)
+        
+    def cb(page):
+        return defer.succeed((page, factory.response_headers))
+    factory.deferred.addCallback(cb)
+    return factory.deferred
 
 def get_headers(url):
-    request = urllib2.Request(url)
-    request.get_method = lambda: 'HEAD'
-    http = urllib2.urlopen(request)
-    return http.headers
-
+    def cb(page, header):
+        new_header = {}
+        for k in header:
+            new_header[k] = header[k][0]
+        return defer.succeed(new_header)
+    d = get_page_with_header(url, method='HEAD')
+    d.addCallback(cb)
+    return d
 
 def is_html(headers):
-    return headers['content-type'].lower()[:9] == "text/html"
+    if not "content-type" in headers:
+        return false
+    return headers["content-type"].lower()[:9] == "text/html"
 
-
-def download_headers_and_content_if_html(url):
-    headers = get_headers()
-    content = ''
-    if is_html(headers):
-        content = download(url)
-    return (headers, content)
-
+def convert_bytes(bytes):
+    """
+        copied from http://www.5dollarwhitebox.org/drupal/node/84
+    """
+    bytes = float(bytes)
+    if bytes >= 1099511627776:
+        terabytes = bytes / 1099511627776
+        size = '%.2fT' % terabytes
+    elif bytes >= 1073741824:
+        gigabytes = bytes / 1073741824
+        size = '%.2fG' % gigabytes
+    elif bytes >= 1048576:
+        megabytes = bytes / 1048576
+        size = '%.2fM' % megabytes
+    elif bytes >= 1024:
+        kilobytes = bytes / 1024
+        size = '%.2fK' % kilobytes
+    else:
+        size = '%.2fb' % bytes
+    return size
 
 def download_if_html(url):
     """
     download content, if the mimetype is text/html
     """
-    if is_html(get_headers(url)):
-        return download(url)
-    return ""
-
+    def cb(header):
+        if (is_html(header)):
+            return download(url)
+        else:
+            return defer.succeed("")
+    d = get_headers(url)
+    d.addCallback(cb)
+    return d
 
 def download(url, file=None, **kwargs):
     """
