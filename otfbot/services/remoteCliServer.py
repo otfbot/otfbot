7cb7b0699b75d28c5c08de959a792ff915854cd6
cato- <otfbot@xenim.de>
2012-04-12 21:38:53 +0200

    Quick fixed controlTCPMod
    * password is now loaded from a file with the format "user:pass"

--- otfbot/services/remoteCliServer.py
+++ otfbot/services/remoteCliServer.py
@@ -27,6 +27,8 @@ from twisted.conch.ssh import factory, userauth, connection, keys, session, comm
 from twisted.conch.insults import insults 
 from twisted.internet.protocol import ProcessProtocol
 
+from twisted.conch import manhole_tap
+
 from zope.interface import implements
 
 from otfbot.lib.pluginSupport import pluginSupport
@@ -50,35 +52,50 @@ class botService(service.MultiService):
 
     def startService(self):
         self.config=self.parent.getServiceNamed("config")
-        f = factory.SSHFactory()
-        f.portal = portal.Portal(SSHRealm(self))
-        f.publicKeys = {'ssh-rsa': keys.Key.fromString('ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV')}    
-        privateKey = """-----BEGIN RSA PRIVATE KEY-----
-MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
-4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
-vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
-Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
-xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
-PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
-gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
-DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
-pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
-EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
------END RSA PRIVATE KEY-----"""
-        f.privateKeys = {'ssh-rsa' : keys.Key.fromString(privateKey) }
-        del privateKey
-        try:
-            f.portal.checkers = self.root.getServiceNamed("auth").getCheckers()
-        except KeyError:
-            pass
-        serv=internet.TCPServer(
-            int(self.config.get("port", 5022, "controlTCPMod")),
-            f, 
-            interface=self.config.get("interface", "127.0.0.1", "controlTCPMod")
-        )
-        serv.setName("server")
-        self.addService(serv)
-        service.MultiService.startService(self)        
+        
+        port = self.config.get("port", 5022, "controlTCPMod")
+        passwd = self.config.get("passwd", "data/passwd", "controlTCPMod")
+        control = self.parent.getServiceNamed("control").handle_command
+        manholeService=manhole_tap.makeService({
+                'telnetPort': '',
+                'sshPort': str(port),
+                'passwd': passwd,
+                'namespace': {'app': self.parent, 'c': control}
+              })
+        manholeService.setName("manhole")
+        self.addService(manholeService)
+
+
+        
+#        f = factory.SSHFactory()
+#        f.portal = portal.Portal(SSHRealm(self))
+#        f.publicKeys = {'ssh-rsa': keys.Key.fromString('ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV')}    
+#        privateKey = """-----BEGIN RSA PRIVATE KEY-----
+#MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
+#4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
+#vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
+#Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
+#xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
+#PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
+#gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
+#DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
+#pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
+#EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
+#-----END RSA PRIVATE KEY-----"""
+#        f.privateKeys = {'ssh-rsa' : keys.Key.fromString(privateKey) }
+#        del privateKey
+#        try:
+#            f.portal.checkers = self.root.getServiceNamed("auth").getCheckers()
+#        except KeyError:
+#            pass
+#        serv=internet.TCPServer(
+#            int(self.config.get("port", 5022, "controlTCPMod")),
+#            f, 
+#            interface=self.config.get("interface", "127.0.0.1", "controlTCPMod")
+#        )
+#        serv.setName("server")
+#        self.addService(serv)
+        service.MultiService.startService(self)
 
 class remoteCLI(pluginSupport, recvline.RecvLine):
     def __init__(self, user):
